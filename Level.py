6034ad89d115a8572b9946fa8122badb90b81ff4
Ix <ixahedron@users.noreply.github.com>
2016-07-06 14:49:03 +0200

    Verschiedene Levels für Multi- und Singleplayer

--- Level.py
+++ Level.py
@@ -350,3 +350,46 @@ class Level_M_01(Level):
     self.exit.sprite = Exit()
     self.exit.sprite.rect.x = 1600
     self.exit.sprite.rect.y = 0.3 * screen_y
+
+
+class Level_01(Level):
+
+  def __init__(self, player, player2 = None):
+    Level.__init__(self, player, player2)
+
+    self.background = pygame.transform.scale(pygame.image.load(bg_image_01), window_size).convert()
+    
+    # every platform: width, height, x, y
+    level = [
+             [20, screen_y, -300, 0],
+             [1900, 20, -300, 0],
+             [1900, 20, -300, screen_y - 20],
+             [1210, 10, 50, 500],
+             [10, 130, 700, 400],
+             [1210, 10, 50, 280],
+             [10, 310, 1400, 180],
+             [exit_width + 20, int(0.3 * screen_y), 1580, 0],
+             [exit_width + 20, int(0.7 * screen_y - exit_height), 1580, int(0.3 * screen_y + exit_height)]
+             ]
+
+    self.left_border = level[0][2]
+
+    cubes = [(60, 450),
+             (100, 510)]
+
+    buttons = [
+               (100, 500),
+       #        (500, 280)
+               ]
+    doors = [
+             (200, 500),
+        #     (1570, 0.50 * screen_y)            
+            ]
+
+    self.init_platforms(level)
+    self.init_cubes(cubes)
+    self.init_buttons(buttons, doors)
+    
+    self.exit.sprite = Exit()
+    self.exit.sprite.rect.x = 1600
+    self.exit.sprite.rect.y = 0.3 * screen_y
